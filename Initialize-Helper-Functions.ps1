f64b65b8f403e308f18fbb1c7eae9b33190286e7
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-03-27 23:35:38 -0700

    Rename $file to $configFile in helper functions Better describes the value of the variable

--- Initialize-Helper-Functions.ps1
+++ Initialize-Helper-Functions.ps1
@@ -26,94 +26,94 @@ function normalize_path($absPath, $relPath) {
 }
 
 ## Writes to a particular Cmder config
-function write_to_config($var, $file, $content) {
-  if($file -eq $var.allConfig) {
+function write_to_config($var, $configFile, $content) {
+  if($configFile -eq $var.allConfig) {
     write_to_config $var $var.bashConfig $content
     write_to_config $var $var.psConfig $content
     write_to_config $var $var.cmdConfig $content
     return
   }
-  # Add-Content -Path $file -Value $content -Encoding "ASCII"
-  "$content" | Out-File -Encoding "ASCII" -Append -FilePath $file  
+  # Add-Content -Path $configFile -Value $content -Encoding "ASCII"
+  "$content" | Out-File -Encoding "ASCII" -Append -FilePath $configFile  
 }
 
 ## Writes to a particular Cmder config, but more suited to a specific case (comment, variable, path, alias)
-function write_comment_to_config($var, $file, $content) {
-  if($file -eq $var.allConfig) {
+function write_comment_to_config($var, $configFile, $content) {
+  if($configFile -eq $var.allConfig) {
     write_comment_to_config $var $var.bashConfig $content
     write_comment_to_config $var $var.psConfig $content
     write_comment_to_config $var $var.cmdConfig $content
     return
   }
-  elseif($file -eq $var.bashConfig) {
+  elseif($configFile -eq $var.bashConfig) {
     write_to_config $var $var.bashConfig "# $content"
   }
-  elseif($file -eq $var.psConfig) {
+  elseif($configFile -eq $var.psConfig) {
     write_to_config $var $var.psConfig "# $content"
   }
-  elseif($file -eq $var.cmdConfig) {
+  elseif($configFile -eq $var.cmdConfig) {
     write_to_config $var $var.cmdConfig ":: $content"
   }
-  print_info "comment" "Adding `"$($content.Substring(0, 15))...`" to `"$(Split-Path $file -Leaf)`""
+  print_info "comment" "Adding `"$($content.Substring(0, 15))...`" to `"$(Split-Path $configFile -Leaf)`""
 }
 
-function write_variable_to_config($var, $file, $variableName, $variableValue) {
-  if($file -eq $var.allConfig) {
+function write_variable_to_config($var, $configFile, $variableName, $variableValue) {
+  if($configFile -eq $var.allConfig) {
     write_variable_to_config $var $var.bashConfig $variableName $variableValue
     write_variable_to_config $var $var.psConfig $variableName $variableValue
     write_variable_to_config $var $var.cmdConfig $variableName $variableValue
     return
   }
-  elseif($file -eq $var.bashConfig) {
-    write_to_config $var $file "$variableName=`"$variableValue`""
+  elseif($configFile -eq $var.bashConfig) {
+    write_to_config $var $configFile "$variableName=`"$variableValue`""
   }
-  elseif($file -eq $var.psConfig) {
-    write_to_config $var $file "`$$variableName=`"$variableValue`""
+  elseif($configFile -eq $var.psConfig) {
+    write_to_config $var $configFile "`$$variableName=`"$variableValue`""
   }
-  elseif($file -eq $var.cmdConfig) {
-    write_to_config $var $file "set $variableName=$variableValue"
+  elseif($configFile -eq $var.cmdConfig) {
+    write_to_config $var $configFile "set $variableName=$variableValue"
   }
-  print_info "variable" "Setting `"$variableName`" to `"$variableValue`" for `"$(Split-path $file -Leaf)`""
+  print_info "variable" "Setting `"$variableName`" to `"$variableValue`" for `"$(Split-path $configFile -Leaf)`""
 }
 
-function write_path_to_config($var, $file, $binName, $content) {
-  if($file -eq $var.allConfig) {
+function write_path_to_config($var, $configFile, $binName, $content) {
+  if($configFile -eq $var.allConfig) {
     write_path_to_config $var $var.bashConfig $binName $content
     write_path_to_config $var $var.psConfig $binName $content
     write_path_to_config $var $var.cmdConfig $binName $content
     return
   }
-  elseif($file -eq $var.bashConfig) {
+  elseif($configFile -eq $var.bashConfig) {
     write_to_config $vars $var.bashConfig "export PATH=\$((Convert-Path $content)):`$PATH".Replace(":\", "\").Replace("\", "/")
   }
-  elseif($file -eq $var.psConfig) {
+  elseif($configFile -eq $var.psConfig) {
     write_to_config $vars $var.psConfig "`$env:Path = `"${content};`" + `$env:Path"
   }
-  elseif($file -eq $var.cmdConfig) {
+  elseif($configFile -eq $var.cmdConfig) {
     write_to_config $var $var.cmdConfig "set PATH=${content};%PATH%"
   }
-  print_info "path" "Adding `"$binName`" to PATH for `"$(Split-Path $file -Leaf)`""
+  print_info "path" "Adding `"$binName`" to PATH for `"$(Split-Path $configFile -Leaf)`""
 }
 
-function write_alias_to_config($var, $file, $aliasName, $aliasValue) {
-  if($file -eq $var.allConfig) {
+function write_alias_to_config($var, $configFile, $aliasName, $aliasValue) {
+  if($configFile -eq $var.allConfig) {
     write_alias_to_config $var $var.bashConfig $aliasName $aliasValue
     write_alias_to_config $var $var.psConfig $aliasName $aliasValue
     write_alias_to_config $var $var.cmdConfig $aliasName $aliasValue
     return
   }
-  elseif($file -eq $var.bashConfig) {
-    write_to_config $var $file "alias $aliasName=`"$aliasValue`""
+  elseif($configFile -eq $var.bashConfig) {
+    write_to_config $var $configFile "alias $aliasName=`"$aliasValue`""
   }
-  elseif($file -eq $var.psConfig) {
-    write_to_config $var $file "function fn-$aliasName { $aliasValue }"
-    write_to_config $var $file "Set-Alias -Name `"$aliasName`" -Value `"fn-$aliasName`""
+  elseif($configFile -eq $var.psConfig) {
+    write_to_config $var $configFile "function fn-$aliasName { $aliasValue }"
+    write_to_config $var $configFile "Set-Alias -Name `"$aliasName`" -Value `"fn-$aliasName`""
   }
-  elseif($file -eq $var.cmdConfig) {
+  elseif($configFile -eq $var.cmdConfig) {
     # For now, aliases are not included for batch
     # Append to pre-created user_aliases.cmd
-    # Write-Line-To-File "alias $alias=$aliasValue" $file
-    # Write-Line-To-File "doskey $alias=$aliasValue" $file
+    # Write-Line-To-File "alias $alias=$aliasValue" $configFile
+    # Write-Line-To-File "doskey $alias=$aliasValue" $configFile
   }
-  print_info "alias" "Setting `"$aliasName`" to `"$aliasValue`" for `"$(Split-Path -Path $file)`""
+  print_info "alias" "Setting `"$aliasName`" to `"$aliasValue`" for `"$(Split-Path -Path $configFile)`""
 }
