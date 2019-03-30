f1c8e7af2b30b4f2ac27a53d878cb30d0a1784fc
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-03-30 15:19:00 -0700

    Setup so config json always created - If portable.config.json not found, use default.config.json - If json from any of those files are invalid, create a blank object

--- Explicitize-Config.ps1
+++ Explicitize-Config.ps1
@@ -1,19 +1,29 @@
 function global_json() {
-    # print_info "`$json" "Creating global object"
-    $configFile = "./new.portable.config.json"
-    # todo: fix bug where portable.config.json is found but does not contain an object
-    if(Test-Path -Path $configFile) {
-        $json = Get-Content -Path $configFile | ConvertFrom-Json
-        fill_default_json_props $json
-        $json
-        return
-    }
-    $json = New-Object -TypeName PsObject
-    fill_default_json_props $json
+  $configJson = New-Object -TypeName PsObject
+  if(Test-Path -Path "./portable.config.json") {
+    print_info "config" "portable.config.json found. Using portable.config.json"
+
+    $configJson = Get-Content -Path "./portable.config.json" | ConvertFrom-Json
+  }
+  # If no portable config found use default.config.json
+  else {
+    print_info "config" "portable.config.json not found. Using default.config.json"
+
+    $configJson = Get-Content -Path "./default.config.json" | ConvertFrom-Json
+  }
+  
+  if($configJson | ConvertTo-Json | Test-Json) {
+    $configJson = $configJson 
+  }
+  else {
+    print_error "config" "Config not valid JSON. Creating blank configuration object"
+    $configJson = New-Object -TypeName PsObject
+  }
+
+  $configJson | ConvertTo-Json | Out-File -FilePath "./abstraction.config.json" -Encoding "ASCII"
 }
 
 function global_vars() {
-    print_info "`$vars" "Creating global object"
-    $b = New-Object -TypeName PsObject
-    print_warning "e" $b
+  print_info "`$vars" "Creating global object"
+  New-Object -TypeName PsObject
 }
