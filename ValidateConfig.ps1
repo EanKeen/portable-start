390cc214a8bf8d9fba09cc3fc2d3563da48ea623
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-05-24 20:01:28 -0700

    fix: stop program with encounter invalid paths as opposed to prompting the uesr if they want to create the path that was not found.

--- ValidateConfig.ps1
+++ ValidateConfig.ps1
@@ -1,45 +1,13 @@
 # Check to be sure all paths user is entering is correct
 
-function prompt_to_create_path($pathName, $pathValue) {
-  print_info "prompt_to_create_path" "Would you like to create a *folder* at `"$pathValue`" for `"$pathName`"?"
-  $key = $Host.UI.RawUI.ReadKey()
-  Write-Host "`r`n"
-  if($key.Character -eq "y") { }
-  elseif($key.Character -eq "n") {
-    print_error "prompt_to_create_path" "Please create the item at `"$pathValue`" for `"$pathName`""
-    exit_program
-  }
-  else { prompt_to_create_path $pathName $pathValue }
-
-  if(Test-Path -Path $pathValue -IsValid) {
-    # Write-Host (Test-Path -Path $pathValue -PathType Container)
-    ## Not sure why this doesn't work. TODO: Fix
-    if(Test-Path -Path $pathValue -PathType Leaf) {
-      New-Item -Path $pathValue -ItemType File -Force | Out-Null
-    }
-    elseif(Test-Path -Path $pathValue -PathType Container) {
-      Write-Host "thing"
-      New-Item -Path $pathValue -ItemType Directory -Force | Out-Null
-    }
-    else {
-      New-Item -Path $pathValue -ItemType Directory -Force | Out-Null
-    }
-    print_info "prompt_to_create_path" "creating path `"$pathName`" at `"$pathValue`""
-  }
-  else {
-    print_error "prompt_to_create_path" "Could not create item for $pathName at $pathValue. String not a valid path reference."
-    exit_program
-  }
-}
-
 function check_path_exists($pathName, $pathValue) {
   if (Test-Path -Path $pathValue) {
     print_info "check_path_exists" "path `"$pathName`" at `"$pathValue`" reference exists"
     return
   }
   else {
-    print_error "check_path_exists" "path `"$pathName`" at `"$pathValue`" reference does not exist"
-    prompt_to_create_path $pathName $pathValue
+    print_error "check_path_exists" "path `"$pathName`" at `"$pathValue`" reference does NOT exist. Create the path or edit your config object"
+    exit_program
   }
 }
 
