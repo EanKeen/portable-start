537e6df7ed531f9bef1515a6b9a81d41c5d4c98c
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-05-27 18:41:23 -0700

    refactor: cleanup prevalidation file

--- PreValidateConfig.ps1
+++ PreValidateConfig.ps1
@@ -1,24 +1,5 @@
-function prevalidate_applications($applications) {
-  foreach($app in $applications) {
-    if($app | obj_not_has_prop "path") {
-      print_error "fill_applications" "An application specified does not have the `"path`" property. Add this required property to `"$app`"."
-      exit_program
-    }
-  }
-}
-
-function prevalidate_binaries($binaries) {
-  foreach($bin in $binaries) {
-    if($bin | obj_not_has_prop "path") {
-      print_error "fill_binaries" "A binary specified does not have the `"path`" property. Add this required property to `"$app`"."
-      exit_program
-    }
-  }
-}
-
-# TODO: // Check for duplication
-function prevalidate_aliases($aliases) {
-  foreach($alias in $aliases) {
+function prevalidate_aliases($config) {
+  foreach($alias in $config.aliases) {
     if($alias | obj_not_has_prop "name") {
       print_error "prevalidate_aliases" "Alias must have property `"name`""
       exit_program
@@ -31,12 +12,36 @@ function prevalidate_aliases($aliases) {
   }
 }
 
-# Validates the user's config *before* any transformations
+function prevalidate_applications($config) {
+  # All props exist
+  foreach($app in $config.applications) {
+    if($app | obj_not_has_prop "path") {
+      print_error "fill_applications" "An application specified does not have the `"path`" property. Add this required property to `"$app`"."
+      exit_program
+    }
+  }
+}
+
+function prevalidate_binaries($config) {
+  # All props exist
+  foreach($bin in $config.binaries) {
+    if($bin | obj_not_has_prop "path") {
+      print_error "fill_binaries" "A binary specified does not have the `"path`" property. Add this required property to `"$app`"."
+      exit_program
+    }
+  }
+}
+
+function prevalidate_refs($config) {
+
+}
+
 # // TODO: Verify json is legit and valid before validating
 function prevalidate_config() {
   $config = Get-Content -Path "./portable.config.json" -Raw | ConvertFrom-Json
 
-  prevalidate_applications $config.applications
-  prevalidate_binaries $config.binaries
-  prevalidate_aliases $config.aliases
+  prevalidate_applications $config
+  prevalidate_binaries $config
+  prevalidate_aliases $config
+  prevalidate_refs $config
 }
