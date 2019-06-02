c392ce62e625827544f995d51f841b9c70a2f290
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-06-01 19:15:37 -0700

    polish: cleanup titles logged to console

--- ExecutePortableScripts.ps1
+++ ExecutePortableScripts.ps1
@@ -1,27 +1,27 @@
 # LOAD HELPER FUNCTIONS
-Write-Host "Load helper functions" -BackgroundColor White -ForegroundColor Black
+Write-Host "Load Helpers" -BackgroundColor White -ForegroundColor Black
 . ./util/PrintToConsole.ps1
 . ./util/WriteToConfig.ps1
 . ./util/General.ps1
 . ./util/GenerateConfigUtil.ps1
 
 # PRE-GEN VALIDATE CONFIG
-print_title "Prevalidate config object"
+print_title "Prevalidate Config"
 . ./PrevalidateConfig.ps1
 prevalidate_config
 
 # GENERATE CONFIG OBJECT
-print_title "Create basic config"
+print_title "Generate Config"
 . ./GenerateConfig.ps1
 Set-Variable -Name "config" -Value $(generate_config) -Scope Private
 
-# VALIDATE CONFIG
-print_title "Validate config object"
+VALIDATE CONFIG
+print_title "Validate Config"
 . ./ValidateConfig.ps1
 validate_config $config
 
 # GENERATE VARS OBJECT
-print_title "Create basic vars"
+print_title "Generate Vars"
 . ./GenerateVars.ps1
 Set-Variable -Name "vars" -Value $(generate_vars $config) -Scope Private
 
@@ -31,7 +31,7 @@ attempt_to_run_hook $vars "portable_hook_after_create_variables `$config `$var"
 
 # CREATE STUFF FOR SCOOP
 if($vars.isUsing.opts.scoopDriveName) {
-  print_title "Create Scoop variables"
+  print_title "Configure Scoop"
   . ./ConfigureScoop.ps1
   configure_scoop $vars $config
   attempt_to_run_hook $vars "portable_hook_after_set_scoop_env_vars `$config `$var"
@@ -39,15 +39,15 @@ if($vars.isUsing.opts.scoopDriveName) {
 
 # CREATE CMDER CONFIG FILES
 if($vars.isUsing.refs.cmderConfigDir) {
-  print_title "Create Cmder config files"
+  print_title "Create Cmder Config"
   . ./CreateCmderConfig.ps1
   cmder_config_write $vars $config
   attempt_to_run_hook $vars "portable_hook_after_create_cmder_files `$config `$var"
 }
 
 # LAUNCH APPLICATIONS
-print_title "Launch applications"
 if($vars.isUsing.refs.appDir) {
+  print_title "Launch Applications"
   . ./LaunchApplications
   prompt_to_launch_apps $vars $config
   attempt_to_run_hook $vars "portable_hook_after_launch_apps `$config `$var"
